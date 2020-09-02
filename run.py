45db27b6449c7fd5b681fccc4c6b64bad6f60df8
deanlicious <dean.w1993@gmail.com>
2020-09-02 22:30:02 +0100

    updated the blit_text to return current words and changed typing to the long text

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ fontSize = 32
 font = pygame.font.Font(None, fontSize)
 
 DisplayWidth = 600
-DisplayHeight = 300
+DisplayHeight = 600
 
 DISPLAYSURF = pygame.display.set_mode((DisplayWidth, DisplayHeight), pygame.RESIZABLE)
 DISPLAYSURF.fill(WHITE)
@@ -50,26 +50,35 @@ words = {
 dean = {
     'correct': []
 }
+
+longtext = "This is a a break " \
+       "in the screen.\nIt can look strange sometimes.\n" \
+       "e surface though, so sometimes " \
+       "text urface"
+
 n = 0
 text = ''
 previousUserWords = ['']
-wordSelect = random.choice(list(words.keys()))
-nextWord = random.choice(list(words.keys()))
+wordSelect = longtext.split(' ')[0]
 
 
 
-def blit_text(surface, text, pos, font, currentUserLocation, color=pygame.Color('black')):
+
+def blit_text(surface, text, pos, font, currentuserlocation, color=pygame.Color('black')):
     #https://stackoverflow.com/questions/42014195/rendering-text-with-multiple-lines-in-pygame
     words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
     space = font.size(' ')[0]  # The width of a space.
     max_width, max_height = surface.get_size()
     x, y = pos
     upto = 0
+    currentword = []
     for line in words:
         for word in line:
             upto += 1
-            if upto > currentUserLocation:
+            if upto > currentuserlocation:
+                currentword.append(word)
                 color = pygame.Color('green')
+
             word_surface = font.render(word, 0, color)
             word_width, word_height = word_surface.get_size()
             if x + word_width >= max_width:
@@ -79,16 +88,20 @@ def blit_text(surface, text, pos, font, currentUserLocation, color=pygame.Color(
             x += word_width + space
         x = pos[0]  # Reset the x.
         y += word_height  # Start on new row.
+    if currentWord:
+        return currentword[0]
+    else:
+        return False
 
-longtext = "This is a really long sentence with a couple of breaks.\nSometimes it will break even if there isn't a break " \
-       "in the sentence, but that's because the text is too long to fit the screen.\nIt can look strange sometimes.\n" \
-       "This function doesn't check if the text is too high to fit on the height of the surface though, so sometimes " \
-       "text will disappear underneath the surface"
 
 
-previousWordColour = RED
 
-currentUserLocation = 10
+
+
+
+
+currentUserLocation = 0
+start = time.process_time()
 
 while True:  # making a loop
 
@@ -98,46 +111,35 @@ while True:  # making a loop
             sys.exit()
         elif event.type == pygame.KEYDOWN:
 
-            if event.key in (pygame.K_RETURN, pygame.K_BACKSPACE, pygame.K_SPACE):
-                currentUserLocation += 1
-                if text.lower() == wordSelect.lower():
-                    previousWordColour = GREEN
-                else:
-                    previousWordColour = RED
-                previousUserWords.append(text)
-                text = ''
-                wordSelect = nextWord
-                nextWord = random.choice(list(words.keys()))
+            if event.key in (pygame.K_RETURN, pygame.K_SPACE):
 
 
+                if text == currentWord:
+                    currentUserLocation += 1
+                    text = ''
+
+
+            elif event.key ==  pygame.K_BACKSPACE:
+                if len(text)>0:
+                    text = text[0:-1]
 
             else:
                 text += event.unicode
-                if text.lower() not in wordSelect.lower():
-                    previousUserWords.append(text)
-                    text = ''
-                    wordSelect = nextWord
-                    nextWord = random.choice(list(words.keys()))
-                    previousWordColour = RED
-
-
-
-
 
 
 
     DISPLAYSURF.fill((WHITE))
-    txt_surface = font.render(text, True, color)
-    txt_previous = font.render(previousUserWords[-1],True,previousWordColour)
-    txt_spell_word = font.render(wordSelect,True,color)
-    txt_nextWord = font.render(nextWord,True,color)
+    currentWord = blit_text(DISPLAYSURF, longtext, (20,20),font,currentUserLocation)
+    txt_current = font.render(text, True, color)
+    txt_spell_word = font.render(currentWord,True,color)
 
-    blit_text(DISPLAYSURF, longtext, (20,20),font,currentUserLocation)
 
-    DISPLAYSURF.blit(txt_surface,(DisplayWidth//2,DisplayHeight//2))
-    DISPLAYSURF.blit(txt_previous,(DisplayWidth//20,DisplayHeight//2))
-    DISPLAYSURF.blit(txt_spell_word, (DisplayWidth // 2, DisplayHeight // 3))
-    DISPLAYSURF.blit(txt_nextWord, (3* DisplayWidth // 4, DisplayHeight // 3))
+
+
+    DISPLAYSURF.blit(txt_current,(DisplayWidth//2,5*DisplayHeight//6))
+
+    DISPLAYSURF.blit(txt_spell_word, (DisplayWidth // 2, 2*DisplayHeight // 3))
+
 
     pygame.display.flip()
     FramePerSec.tick(FPS)
