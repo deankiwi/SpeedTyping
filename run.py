409beb259a0a8531994db47723587d702588e06c
deanlicious <dean.w1993@gmail.com>
2020-10-04 22:19:11 +0100

    change this data set to a json file. starting to implerment a fresh

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 import pygame
-import numpy as np
+import praw
+from apilogin import *
+import random
 import time
 import cv2
 import sys
 import datetime
 from pygame.locals import *
 import os
+import json
+import pathlib
 
 pygame.init()
 
@@ -13,7 +17,16 @@ FPS = 120
 FramePerSec = pygame.time.Clock()
 cap0 = cv2.VideoCapture(0)
 #cap1 = cv2.VideoCapture(1)
-path = '/data/run'
+
+
+#create a
+completedTests = 0
+
+newpath = str(pathlib.Path(__file__).parent.absolute()) + r'\data\testing'
+while os.path.exists(newpath + str(completedTests) ):
+    completedTests += 1
+newpath = newpath + str(completedTests)
+os.makedirs(newpath)
 
 
 WHITE = (255, 255, 255)
@@ -33,19 +46,55 @@ DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption('Spelling Game')
 
 
-#longtext = "a1a2aqaza s3swsxs d4dedcd f5frfvf6ftfgfbf j7jyjhjnjujm k8kik,k l9lol.l ;0;p;/;-;[;';];="
-longtext = 'this is me testing out camera side by side'
-n = 0
-text = ''
-previousUserWords = ['']
-wordSelect = longtext.split(' ')[0]
-lastimages = []
-
-userkeyboard = 'logitech g810'
-
+subreddit = reddit.subreddit('python')
+reddittitles = []
+hot_python = subreddit.hot()
+for submission in hot_python:
+    if not submission.stickied:
+        removeNonString = ''.join([i if ord(i) < 128 else ' ' for i in submission.title])
+        reddittitles.append(removeNonString)
+longtext = random.choice(reddittitles)
 
 
-def blit_text(surface, text, pos, font, currentuserlocation, color=pygame.Color('black')):
+
+def fresh_game():
+    #set all variable back to normal
+    newpath = str(pathlib.Path(__file__).parent.absolute()) + r'\data\typingtest'
+    i = 0
+    while os.path.exists(newpath + str(completedTests)):
+        completedTests += 1
+    newpath = newpath + str(completedTests)
+    os.makedirs(newpath)
+    currentUserLocation = 0
+    logger = ''
+    jsonData = {
+        'text': longtext,
+        'datetime': datetime.datetime.now(),
+        'typeData': []
+    }
+    start = datetime.datetime.now()
+    text = ''
+    typeDataWord = []
+    filename = newpath + '/spellingQuiz-' + datetime.datetime.now().strftime("%Y-%m-%d@%H#%M#%S") + '.txt'
+    longtext = random.choice(reddittitles)
+    freshgame = {
+        'newpath' : newpath,
+        'currentUserLocation': currentUserLocation,
+        'logger' : logger,
+        'jsonData' : jsonData,
+        'typeDataWord' : typeDataWord,
+        'start' : start,
+        'filename' : filename,
+        'longtext' : longtext,
+        'i' : i,
+        'text' : text
+    }
+    return freshgame
+
+
+
+
+def blit_text(surface, text, pos, font, currentuserlocation, color=pygame.Color('green')):
     #https://stackoverflow.com/questions/42014195/rendering-text-with-multiple-lines-in-pygame
     words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
     space = font.size(' ')[0]  # The width of a space.
@@ -58,7 +107,7 @@ def blit_text(surface, text, pos, font, currentuserlocation, color=pygame.Color(
             upto += 1
             if upto > currentuserlocation:
                 currentword.append(word)
-                color = pygame.Color('green')
+                color = pygame.Color('black')
 
             word_surface = font.render(word, 0, color)
             word_width, word_height = word_surface.get_size()
@@ -83,8 +132,15 @@ def blit_text(surface, text, pos, font, currentuserlocation, color=pygame.Color(
 
 currentUserLocation = 0
 logger = ''
+jsonData = {
+    'text' : longtext,
+    'datetime' : datetime.datetime.now(),
+    'typeData' : []
+}
 
-filename = 'data/spellingQuiz-'+ datetime.datetime.now().strftime("%Y-%m-%d@%H#%M#%S") + '.txt'
+typeDataWord = []
+
+filename = newpath + '/spellingQuiz-'+ datetime.datetime.now().strftime("%Y-%m-%d@%H#%M#%S") + '.txt'
 startgame = True
 
 while startgame:
@@ -98,6 +154,11 @@ while startgame:
                 startgame = False
 
 start = datetime.datetime.now()
+text = ''
+
+refreshgame = fresh_game()
+
+
 
 while True:  # making a loop
     ret, frame = cap0.read()
@@ -118,28 +179,32 @@ while True:  # making a loop
                 if text == currentWord:
                     with open(filename, 'a') as text_file:
                         text_file.write(f'{logger}\n')
+                    jsonData['typeData'].append(typeDataWord)
                     currentUserLocation += 1
                     text = ''
                     logger = ''
+                    typeDataWord = []
 
 
             elif event.key ==  pygame.K_BACKSPACE:
                 if len(text)>0:
-                    logger += f'[{datetime.datetime.now()-start},h DELETE]\t'
+                    logger += f'[{datetime.datetime.now()-start},DELETE]\t'
+                    typeDataWord.append([datetime.datetime.now() - start, 'DELETE'])
                     text = text[0:-1]
 
             else:
                 ret0, frame0 = cap0.read()
                 #ret1, frame1 = cap1.read()
                 if ret0 == True:
-                    img_name = f"data/{int(round(time.time() * 1000))}{event.unicode}"
-                    img_name0 = img_name +'a.png'
+                    img_name = f"{newpath}/{int(round(time.time() * 1000))}{event.unicode}"
+                    img_name0 = img_name +'.png'
                     #img_name1 = img_name + 'b.png'
                     cv2.imwrite(img_name0, frame0)
                     #scv2.imwrite(img_name1, frame1)
 
 
                 logger += f'[{datetime.datetime.now()-start}, {event.unicode}]\t'
+                typeDataWord.append([datetime.datetime.now()-start, event.unicode])
                 text += event.unicode
 
 
@@ -157,8 +222,14 @@ while True:  # making a loop
 
     if currentWord == '':
         print(f'total time: {datetime.datetime.now()-start}')
+        with open(newpath+r'\dataSet' +  '.json','w+', encoding='utf-8' ) as f:
+            print(jsonData)
+            json.dump(jsonData, f, ensure_ascii=False, indent=4 , default=str)
         pygame.quit()
         sys.exit()
+
+
+
 
 
 cap0.release()
